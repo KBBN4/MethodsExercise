e9a5cc46305dbec0fea5d6ad007947471eda5d18
katherine Buendia <kbbuendia24@gmail.com>
2023-04-10 12:49:10 -0400

    A Short story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color ?");
+            string color = Console.ReadLine();
+            Console.WriteLine(" What is your favorite animal ?");
+            string Animal = Console.ReadLine();
+            Console.WriteLine(" What is your favorite band?");
+            string Band = Console.ReadLine();
+
+            Console.WriteLine($" Hi,this story is about {name}.{name} favorite color is {color}.\n I think thats a lie though. Not sure." +
+                $"i then asked about her favorite animal. She said her favorite animal is a {Animal}. Can you belive that ! a, {Animal}.\n " +
+                $"i was so shocked by the answers so,of course i then asked , tell me your favorite band and she replied with {Band}." +
+                $"Oh, no, no no. Unbelievable.");
         }
     }
 }
